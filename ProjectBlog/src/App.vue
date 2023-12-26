90b3e70080f960966d7ec6d1ed1358ed39a32473
TranHuuAn-238 <tranhuuan2308@gmail.com>
2023-12-26 21:13:20 +0700

    C6 blog tin tuc: xay dung cau truc ung dung

---
+++ ProjectBlog/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+	<div id="app">
+		<navigation />
+
+		<about-us />
+
+		<comp-footer />
+	</div>
+</template>
+
+<script>
+import Navigation from "./components/Navigation";
+import CompFooter from "./components/CompFooter";
+
+import AboutUs from "./pages/AboutUs";
+export default {
+	name: 'app',
+	components: {
+		Navigation,
+		CompFooter,
+		AboutUs
+	},
+	data () {
+		return {
+			
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
