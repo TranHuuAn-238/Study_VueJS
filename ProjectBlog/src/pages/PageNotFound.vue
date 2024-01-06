2d8922db90a2070e850119e661c848b92fe6e122
TranHuuAn-238 <tranhuuan2308@gmail.com>
2024-01-06 22:21:24 +0700

    C6 blog tin tuc: xay dung page login va gioi han vung truy cap

---
+++ ProjectBlog/src/pages/PageNotFound.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="page-404">
+        <div>
+            <img src="https://media.geeksforgeeks.org/wp-content/uploads/20230802153215/Error-404.png" alt="404">
+        </div>
+        <router-link 
+            tag="button"
+            to="/"
+            type="submit" class="btn btn-primary">Go to HomePage</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'page-not-found'
+}
+</script>
+
+<style>
+    .page-404 {
+        text-align: center;
+    }
+</style>
--- ProjectBlog/src/router.js
+++ ProjectBlog/src/router.js
@@ -3,8 +3,12 @@ import VueRouter from 'vue-router'
 
 import HomePage from './pages/HomePage';
 import Contact from './pages/Contact';
+import Login from './pages/Login';
 import AboutUs from './pages/AboutUs';
 import BlogDetail from './pages/BlogDetail';
+import PageNotFound from './pages/PageNotFound';
+
+import store from './store';
 
 Vue.use(VueRouter)
 
@@ -27,7 +31,24 @@ const routes = [
     {
         path: '/blog-detail/:title.:id',
         name: 'blog-detail',
-        component: BlogDetail
+        component: BlogDetail,
+        beforeEnter: (to, from, next) => {
+            if (store.state.isLogin) {
+                next();
+            } else {
+                next({ path: '/login', query: { redirect: 'blog-detail' } });
+            }
+        }
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login
+    },
+    {
+        path: '*',
+        name: 'not-found',
+        component: PageNotFound
     }
 ]
 
